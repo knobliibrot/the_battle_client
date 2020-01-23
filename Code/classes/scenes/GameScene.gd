7f4b05aa895214d1c80c6b14100463ebffe6bde4
Sam <methy@hotmail.ch>
2020-01-23 09:42:47 +0100

    responisive

--- Code/classes/scenes/GameScene.gd
+++ Code/classes/scenes/GameScene.gd
@@ -3,21 +3,21 @@ extends "res://classes/scenes/ScreenScene.gd"
 signal game_over
 
 func _ready():
-	$Playground/Gamelogic.initialize_game(PlayerTypeEnum.MANUAL, PlayerTypeEnum.MANUAL)
-	$Playground/Gamelogic.generate_battelfield()
+	$Content/Playground/Gamelogic.initialize_game(PlayerTypeEnum.MANUAL, PlayerTypeEnum.MANUAL)
+	$Content/Playground/Gamelogic.generate_battelfield()
 	
-	$Playground/Gamelogic.choose_castel(true)
-	yield($Playground/Gamelogic, "castle_set")
-	$Playground/Gamelogic.choose_castel(false)
-	yield($Playground/Gamelogic, "castle_set")
+	$Content/Playground/Gamelogic.choose_castel(true)
+	yield($Content/Playground/Gamelogic, "castle_set")
+	$Content/Playground/Gamelogic.choose_castel(false)
+	yield($Content/Playground/Gamelogic, "castle_set")
 	
 	var game_finished: bool = false
 	while(!game_finished):
-		$Playground/Gamelogic.start_turn(true)
-		game_finished = yield($Playground/Gamelogic, "turn_finished")
+		$Content/Playground/Gamelogic.start_turn(true)
+		game_finished = yield($Content/Playground/Gamelogic, "turn_finished")
 		if !game_finished:
-			$Playground/Gamelogic.start_turn(false)
-			game_finished = yield($Playground/Gamelogic, "turn_finished")
+			$Content/Playground/Gamelogic.start_turn(false)
+			game_finished = yield($Content/Playground/Gamelogic, "turn_finished")
 	
 	emit_signal("game_over")
 
